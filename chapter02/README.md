4785e3ee10777407d874d84e5f04c8a0174f8da1
Takashi Suzuki <takashi.suzuki@dena.com>
2019-07-21 04:09:44 +0900

    add chapter02

---
+++ chapter02/README.md
@@ -0,0 +1,21 @@
+# chapter02
+
+
+# xx
+```zsh
+$ python relay_module.py --help
+usage: relay_module.py [-h] [-c CHANNEL] [-w WAIT_TIME] [-i INTERVAL]
+
+Relay Module Script
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -c CHANNEL, --channel CHANNEL
+                        set channel (default 7 channel)
+  -w WAIT_TIME, --wait-time WAIT_TIME
+                        set power on wait seconds (default 3 seconds)
+  -i INTERVAL, --interval INTERVAL
+                        set script interval seconds (default 10 seconds)
+
+$ python relay_module.py -w 5 -i 5
+```
---
+++ chapter02/relay_module.py
@@ -0,0 +1,57 @@
+from time import sleep
+import RPi.GPIO as GPIO
+import argparse
+
+DEFAULT_CHANNEL = 7
+DEFAULT_WAIT_TIME = 3
+DEFAULT_INTERVAL_TIME = 10
+
+
+def init():
+    GPIO.setwarnings(False)
+    GPIO.setmode(GPIO.BOARD)
+
+
+def power_on(channel, interval):
+    GPIO.setup(channel, GPIO.OUT)
+    GPIO.output(channel, 1)
+    sleep(interval)
+    GPIO.output(channel, 0)
+    GPIO.cleanup()
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Relay Module Script")
+    parser.add_argument(
+        "-c",
+        "--channel",
+        type=int,
+        default=DEFAULT_CHANNEL,
+        help="set channel (default {} channel)".format(DEFAULT_CHANNEL),
+    )
+    parser.add_argument(
+        "-w",
+        "--wait-time",
+        type=int,
+        default=DEFAULT_WAIT_TIME,
+        help="set power on wait seconds (default {} seconds)".format(
+            DEFAULT_WAIT_TIME),
+    )
+    parser.add_argument(
+        "-i",
+        "--interval",
+        type=int,
+        default=DEFAULT_INTERVAL_TIME,
+        help="set script interval seconds (default {} seconds)".format(
+            DEFAULT_INTERVAL_TIME
+        ),
+    )
+    args = parser.parse_args()
+
+    init()
+    sleep(args.wait_time)
+    power_on(args.channel, args.interval)
+
+
+if __name__ == "__main__":
+    main()
