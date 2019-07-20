80a64e5b6a001bca20e2eb0690bcd3870732e0fa
Takashi Suzuki <takashi.suzuki@dena.com>
2019-07-21 05:44:39 +0900

    add chapter03

---
+++ chapter03/README.md
@@ -0,0 +1,59 @@
+# chapter03
+Let's write to GoogleSpreadSheet.
+
+# Setup
+## Create Service Account
+### By Website
+```zsh
+# create service account
+https://cloud.google.com/iam/docs/creating-managing-service-accounts
+# set gcp path
+$ copy key.json ../.gcp/key.json
+# enable sheet api
+https://cloud.google.com/apis/docs/enable-disable-apis
+```
+
+### By command line
+```zsh
+# login google suite
+$ gcloud auth login
+# create GCP project
+$ gcloud projects create [PROJECT_ID]
+# create service account
+$ gcloud iam service-accounts create [SA-NAME] --display-name [SA-DISPLAY-NAME]
+# generate service account key
+$ gcloud iam service-accounts keys create ../.gcp/key.json --iam-account [SA-NAME]@[PROJECT-ID].iam.gserviceaccount.com
+# enable sheets api
+$ gcloud services enable sheets.googleapis.com
+```
+
+## Add Sheet Permission
+Create a GoogleSpreadSheet and from "Share" add the edit permission of the service account created above.
+
+# Run
+## Write Google Spread Sheet
+```zsh
+$ python spread_sheet.py --help
+usage: spread_sheet.py [-h] [-k KEY_PATH] [-i SPREAD_SHEET_ID] [-l LABEL]
+                       [-t INPUT_TEXT]
+
+Google Spread Sheet Script
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -k KEY_PATH, --key-path KEY_PATH
+                        set service account key path (default
+                        ../.gcp/key.json)
+  -i SPREAD_SHEET_ID, --spread-sheet-id SPREAD_SHEET_ID
+                        set spread sheet id
+  -l LABEL, --label LABEL
+                        set spread sheet cell label (default A1)
+  -t INPUT_TEXT, --input-text INPUT_TEXT
+                        set script interval seconds (default 100 seconds)
+
+
+# Pass SPREAD_SHEET_ID in command line argument
+$ python spread_sheet.py -i SPREAD_SHEET_ID -t 1234
+# or update DEFAULT_SHEET_ID
+$ python spread_sheet.py -t 1234
+```
---
+++ chapter03/spread_sheet.py
@@ -0,0 +1,71 @@
+from oauth2client.service_account import ServiceAccountCredentials
+import argparse
+import gspread
+
+SCOPE = [
+    "https://spreadsheets.google.com/feeds",
+    "https://www.googleapis.com/auth/drive",
+]
+
+DEFAULT_KEY_PATH = "../.gcp/key.json"
+DEFAULT_SHEET_ID = "dummy"
+DEFAULT_SHEET_INDEX = 0
+
+
+class SpreadSheet(object):
+    def __init__(self, key_path, spread_sheet_id):
+        credentials = ServiceAccountCredentials.from_json_keyfile_name(
+            key_path, SCOPE)
+
+        self.sheet_client = gspread.authorize(credentials).open_by_key(spread_sheet_id)
+
+    def get_label_value(self, label, index=DEFAULT_SHEET_INDEX):
+        return self.sheet_client.get_worksheet(index).acell(label).value
+
+    def set_label_value(self, label, value, index=DEFAULT_SHEET_INDEX):
+        self.sheet_client.get_worksheet(index).update_acell(label, value)
+
+
+def main():
+    default_input_text = 100
+    default_label = "A1"
+
+    parser = argparse.ArgumentParser(description="Google Spread Sheet Script")
+    parser.add_argument(
+        "-k",
+        "--key-path",
+        type=str,
+        default=DEFAULT_KEY_PATH,
+        help="set service account key path (default {})".format(DEFAULT_KEY_PATH),
+    )
+    parser.add_argument(
+        "-i",
+        "--spread-sheet-id",
+        type=str,
+        default=DEFAULT_SHEET_ID,
+        help="set spread sheet id",
+    )
+    parser.add_argument(
+        "-l",
+        "--label",
+        type=str,
+        default=default_label,
+        help="set spread sheet cell label (default {})".format(default_label),
+    )
+    parser.add_argument(
+        "-t",
+        "--input-text",
+        type=int,
+        default=default_input_text,
+        help="set script interval seconds (default {} seconds)".format(
+            default_input_text
+        ),
+    )
+    args = parser.parse_args()
+
+    spread_sheet = SpreadSheet(args.key_path, args.spread_sheet_id)
+    spread_sheet.set_label_value(args.label, args.input_text)
+
+
+if __name__ == "__main__":
+    main()
