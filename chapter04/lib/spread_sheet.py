d014d7f1fc79e65671c68fa377bf7839416b6a35
Takashi Suzuki <takashi.suzuki@dena.com>
2019-07-29 22:08:00 +0900

    fix bug for google oauth token cache

--- chapter04/lib/spread_sheet.py
+++ chapter04/lib/spread_sheet.py
@@ -11,17 +11,19 @@ DEFAULT_SHEET_INDEX = 0
 
 class SpreadSheet(object):
     def __init__(self, key_path, spread_sheet_id):
-        credentials = ServiceAccountCredentials.from_json_keyfile_name(
-            key_path, SCOPE)
+        self._key_path = key_path
+        self._spread_sheet_id = spread_sheet_id
 
-        self._sheet_client = gspread.authorize(
-            credentials).open_by_key(spread_sheet_id)
+    def _get_client(self):
+        credentials = ServiceAccountCredentials.from_json_keyfile_name(
+            self._key_path, SCOPE)
+        return gspread.authorize(credentials).open_by_key(self._spread_sheet_id)
 
     def get_label_value(self, label, index=DEFAULT_SHEET_INDEX):
-        return self._sheet_client.get_worksheet(index).acell(label).value
+        return self._get_client().get_worksheet(index).acell(label).value
 
     def set_label_value(self, label, value, index=DEFAULT_SHEET_INDEX):
-        self._sheet_client.get_worksheet(index).update_acell(label, value)
+        self._get_client().get_worksheet(index).update_acell(label, value)
 
     def append_row(self, values, index=DEFAULT_SHEET_INDEX):
-        self._sheet_client.get_worksheet(index).append_row(values)
+        self._get_client().get_worksheet(index).append_row(values)
